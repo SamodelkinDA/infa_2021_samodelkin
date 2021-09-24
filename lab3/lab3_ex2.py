89b5de64b3f9f7c04ba75377dd876cf4b011c0d3
SamodelkinDA <samodelkin.da@phystech.edu>
2021-09-24 18:57:06 +0300

    Добавлен новый файл. Сделана дальняя птица, рыба

--- lab3/lab3_ex2.py
+++ lab3/lab3_ex2.py
@@ -1,5 +1,6 @@
 """Эта программа рисует красивые картинки.
-   На данный момент она может нарисовать далеких птиц, плавник рыбы
+   На данный момент она может нарисовать далеких птиц и
+   глаз, плавник рыбы
 """
 
 
@@ -30,7 +31,7 @@ def fishs_fin(fin_size, fin_tetta, fin_flip_bool):
                       (80, 91),(90, 102),(100, 114),(110, 135), 
                       (115, 155)]
     polygon(fish_fin_surface, (200,100,100), massive_fish_fin)
-    polygon(fish_fin_surface, (0,0,0), massive_fish_fin, 1)
+    polygon(fish_fin_surface, (0,0,0), massive_fish_fin, 4)
     return (pygame.transform.scale(
         pygame.transform.flip(
             pygame.transform.rotate(
@@ -44,10 +45,11 @@ def fishs_fin(fin_size, fin_tetta, fin_flip_bool):
 def fishs_eye(fish_eye_scale):
     fish_eye_surface = pygame.Surface((200, 200), pygame.SRCALPHA)
     fish_eye_surface.fill((0, 0, 0, 0))
-    circle(fish_eye_surface, (0, 0, 250), (100, 100), 50)
-    circle(fish_eye_surface, (10, 30, 30), (110, 110), 20)
     fish_eye_blic_surface = pygame.Surface((100, 100), pygame.SRCALPHA)
     fish_eye_blic_surface.fill((0, 0, 0, 0))
+
+    circle(fish_eye_surface, (0, 0, 250), (100, 100), 50)
+    circle(fish_eye_surface, (10, 30, 30), (110, 110), 20)
     ellipse(fish_eye_blic_surface, (230, 230, 230, 178), [20, 20, 15, 40], 0)
     fish_eye_surface.blit(pygame.transform.rotate(fish_eye_blic_surface, 45), (44, 16))
     return pygame.transform.scale(
@@ -57,13 +59,28 @@ def fishs_eye(fish_eye_scale):
 def draw_fish(x_fish, y_fish, size_of_fish):
     fish_surface = pygame.Surface((200, 200), pygame.SRCALPHA)
     fish_surface.fill((0, 0, 0, 0))
-    fish_surface.blit(fishs_fin(200, 180, 1), (0, 0))
-    fish_surface.blit(fishs_eye(80), (0, 0))
+    fish_surface.blit(fishs_fin(70, -5, 0), (80, 50))
+    fish_surface.blit(fishs_fin(40, 190, 0), (120, 100))
+    fish_surface.blit(fishs_fin(40, 190, 1), (75, 95))
+    massive_fish_body=[(170, 100), (165, 94), (160, 90), (150, 86), (140, 83), 
+                 (130, 81), (120, 81), (110, 83), (100, 86), (90, 90), (80, 94),    
+                 (70, 100), (80, 104), (90, 110), (100, 114), (110, 117), (120, 119),
+                 (130, 119), (140, 117), (150, 114), (160, 110), (165, 106) 
+                 ]
+    massive_fish_tale=[(70, 100), (35, 120), (30, 90), (35, 93), (40, 95), (50, 98), (60, 99) ]
 
+    polygon(fish_surface, (50,100,150), massive_fish_body)
+    polygon(fish_surface, (0,0,0), massive_fish_body, 1)
+    polygon(fish_surface, (200,100,100), massive_fish_tale)
+    polygon(fish_surface, (0,0,0), massive_fish_tale, 1)
+    for i in range(0, 5, 1):
+        line(fish_surface, (0,0,0), (70, 100), (31 + i, 96 + 6*i), 1)
 
+    fish_surface.blit(fishs_eye(30), (130, 85))
+    
     screen.blit(pygame.transform.scale(
         fish_surface, (size_of_fish, size_of_fish)), 
-        (x_fish - 100, y_fish - 100)
+        (x_fish, y_fish)
     )
     return 
 
@@ -72,7 +89,7 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 circle(screen, (100, 100, 100), (200, 200), 2000)
-draw_fish(100, 100, 200)
+draw_fish(0, 0, 200)
 
 
 
